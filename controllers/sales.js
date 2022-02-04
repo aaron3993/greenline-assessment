d05e09cc226b638995a0f8d985fc7cccf23a0953
Aaron <aaron3993@hotmail.com>
2022-02-04 18:19:33 -0500

    attempting to get total sales by company

--- controllers/sales.js
+++ controllers/sales.js
@@ -1,10 +1,31 @@
+const Company = require("../models/Company");
 const Product = require("../models/Product");
 const ProductSale = require("../models/Product-Sale");
 const Sale = require("../models/Sale");
+const sequelize = require("sequelize");
+const User = require("../models/User");
 
 exports.getSales = async (req, res) => {
   try {
     const sales = await Sale.findAll();
+    res.status(200).send(sales);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+// exports.getTotalSalesByCompany = () =>
+//   Sale.findAll({
+//     attributes: [[sequelize.fn("sum", sequelize.col("total")), "totalAmount"]],
+//   });
+
+exports.getTotalSalesByCompany = async (req, res) => {
+  try {
+    const { companyId } = req.params;
+    const users = await User.findAll({ where: { companyId } });
+    const userIds = users.map((user) => user.id);
+    const sales = await Sale.findAll({ where: { userId: userIds } });
+
     res.status(200).send(sales);
   } catch (err) {
     res.status(404).json({ message: err.message });
--- routes/sales.js
+++ routes/sales.js
@@ -4,6 +4,8 @@ const sales = require("../controllers/sales.js");
 
 router.get("/", sales.getSales);
 
+router.get("/total/:companyId", sales.getTotalSalesByCompany);
+
 router.post("/", sales.createSale);
 
 module.exports = router;
