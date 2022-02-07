0a5d1bd5c852bd5c6461a7d2624674a18d6c0aec
Aaron <aaron3993@hotmail.com>
2022-02-07 14:38:19 -0500

    retrieved the total sales by company

--- controllers/sales.js
+++ controllers/sales.js
@@ -8,16 +8,11 @@ const User = require("../models/User");
 exports.getSales = async (req, res) => {
   try {
     const sales = await Sale.findAll();
-    res.status(200).send(sales);
+    res.status(200).json(sales);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
 };
-
-// exports.getTotalSalesByCompany = () =>
-//   Sale.findAll({
-//     attributes: [[sequelize.fn("sum", sequelize.col("total")), "totalAmount"]],
-//   });
 
 exports.getTotalSalesByCompany = async (req, res) => {
   try {
@@ -25,8 +20,8 @@ exports.getTotalSalesByCompany = async (req, res) => {
     const users = await User.findAll({ where: { companyId } });
     const userIds = users.map((user) => user.id);
     const sales = await Sale.findAll({ where: { userId: userIds } });
-
-    res.status(200).send(sales);
+    const total = sales.reduce((a, b) => Number(a.total) + Number(b.total));
+    res.status(200).json(total);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
@@ -52,7 +47,7 @@ exports.createSale = async (req, res) => {
       await sale.save();
       productSales.push(newProductSale);
     }
-    res.status(200).send({ sale, productSales });
+    res.status(200).json({ sale, productSales });
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
